b638515103e23715bb4876112565a09884e8aede
Ren Velitario <velitarioren@gmail.com>
2026-03-11 21:09:39 +0800

    implement apm with site24x7

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,39 @@
-import express from 'express';
-import subjectsRouter from './routes/subjects';
-import cors from 'cors';
-import securityMiddleware from './middleware/security';
+import AgentAPI from "apminsight";
+AgentAPI.config();
+
+import express from "express";
+import subjectsRouter from "./routes/subjects";
+import cors from "cors";
+import securityMiddleware from "./middleware/security";
 import { toNodeHandler } from "better-auth/node";
-import { auth } from './lib/auth';
+import { auth } from "./lib/auth";
 
 const app = express();
-const PORT = Number(process.env.PORT) || 8000
-
+const PORT = Number(process.env.PORT) || 8000;
 
 const FRONTEND_URL = process.env.FRONTEND_URL;
-if (!FRONTEND_URL && process.env.NODE_ENV === 'production') {
-  throw new Error('FRONTEND_URL is required in production');
+if (!FRONTEND_URL && process.env.NODE_ENV === "production") {
+  throw new Error("FRONTEND_URL is required in production");
 }
 
-app.use(cors({
-  origin: FRONTEND_URL ?? 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: FRONTEND_URL ?? "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 
-app.all('/api/auth/*splat', toNodeHandler(auth));
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
-app.use(securityMiddleware)
+app.use(securityMiddleware);
 
-app.use('/api/subjects', subjectsRouter)
+app.use("/api/subjects", subjectsRouter);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Classroom Backend!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Classroom Backend!");
 });
 
 app.listen(PORT, () => {
